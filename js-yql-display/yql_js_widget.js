bee681c1fc1c09dc7a6aba8a925cd30a9dc10afe
Jonathan LeBlanc <joncleblanc@yahoo.com>
2010-04-19 08:54:53 -0700

    encapsulated console.logs in debug & cleaned readme

--- js-yql-display/yql_js_widget.js
+++ js-yql-display/yql_js_widget.js
@@ -10,140 +10,140 @@
 
 //YUI includes for GET utility
 if (! window.YAHOO){
-	document.write('<script type="text/javascript" src="http://yui.yahooapis.com/2.7.0/build/yahoo/yahoo-min.js" ></script>' +
-				   '<script type="text/javascript" src="http://yui.yahooapis.com/2.7.0/build/get/get-min.js" ></script>');
+    document.write('<script type="text/javascript" src="http://yui.yahooapis.com/2.8.0/build/yahoo/yahoo-min.js"></script>' +
+                   '<script type="text/javascript" src="http://yui.yahooapis.com/2.8.0/build/get/get-min.js"></script>');
 }
    
 yqlWidget = function() {
-	//property instantiation
-	var yqlPublicQueryURL = "http://query.yahooapis.com/v1/public/yql?";
-	var widgetStack = [];
-	var currString, resultFormat, queryInsert, setupConfig = [];
-	var regex = /\{([\w\-\.\[\]]+)\}/gi;
+    //property instantiation
+    var yqlPublicQueryURL = "http://query.yahooapis.com/v1/public/yql?";
+    var widgetStack = [];
+    var currString, resultFormat, queryInsert, setupConfig = [];
+    var regex = /\{([\w\-\.\[\]]+)\}/gi;
 	
-	/************************************************************
-	* Method: YUI GET Status Handlers 
-	* Description: YUI GET function status functions
-	************************************************************/
-	var onYQLReqSuccess = function(o){ if (setupConfig['debug'] && window.console){ console.log('GET request succeeded'); }}
-	var onYQLReqFailure = function(o){ if (setupConfig['debug'] && window.console){ console.log('GET request failed'); }}
+    /************************************************************
+    * Method: YUI GET Status Handlers 
+    * Description: YUI GET function status functions
+    ************************************************************/
+    var onYQLReqSuccess = function(o){ if (setupConfig['debug'] && window.console){ console.log('GET request succeeded'); }}
+    var onYQLReqFailure = function(o){ if (setupConfig['debug'] && window.console){ console.log('GET request failed'); }}
     
-	/************************************************************
-	* Method: Get YQL Data
-	* Description: Use the query provided to make a request to 
-	*              YQL endpoint to capture data
-	************************************************************/
-	var getYQLData = function(query){
-		//prepare the URL for YQL query:
+    /************************************************************
+    * Method: Get YQL Data
+    * Description: Use the query provided to make a request to 
+    *              YQL endpoint to capture data
+    ************************************************************/
+    var getYQLData = function(query){
+        //prepare the URL for YQL query:
         var sURL = yqlPublicQueryURL + "q=" + encodeURI(query) + "&format=json&callback=yqlWidget.getYQLDataCallback";
 
-		//add any environment files specified in the config
-		if (setupConfig['env']) {
-			sURL += "&env=" + escape(setupConfig['env']);
-		}
+        //add any environment files specified in the config
+        if (setupConfig['env']) {
+            sURL += "&env=" + escape(setupConfig['env']);
+        }
 		
-		//disable diagnostics if not set to true
-		if (setupConfig['diagnostics'] !== true){
-			sURL += "&diagnostics=false";
-		}
+        //disable diagnostics if not set to true
+        if (setupConfig['diagnostics'] !== true){
+            sURL += "&diagnostics=false";
+        }
 		
-		//make GET request to YQL with provided query
+        //make GET request to YQL with provided query
         var transactionObj = YAHOO.util.Get.script(sURL, {
          	onSuccess : onYQLReqSuccess,
-			onFailure : onYQLReqFailure,
+            onFailure : onYQLReqFailure,
             scope     : this
         });
 		
-		return transactionObj;
+        return transactionObj;
     }
 	
-	/************************************************************
-	* Method: Parse YQL Results
-	* Description: Using the result set, parse the YQL results
-	*			   into display mode
-	************************************************************/
-	var parseYQLResults = function(results){
-		//get first JSON node - use loop due to first node being an unknown object
-		var firstChild;
-		for (var child in results){
-			if (results.hasOwnProperty(child)){
-				firstChild = results[child];
-				break;
-			}
-		}
+    /************************************************************
+    * Method: Parse YQL Results
+    * Description: Using the result set, parse the YQL results
+    *			   into display mode
+    ************************************************************/
+    var parseYQLResults = function(results){
+        //get first JSON node - use loop due to first node being an unknown object
+        var firstChild;
+        for (var child in results){
+            if (results.hasOwnProperty(child)){
+                firstChild = results[child];
+                break;
+            }
+        }
 		
-		//return data instantiation
-		var html = "";
+        //return data instantiation
+        var html = "";
 		
-		//loop through all YQL return elements and result replace regex
-		if (firstChild.length !== undefined){
-			//multiple results - array
-			for(var i = 0; i < firstChild.length; i++){
-				html += parseFormat(firstChild[i]);
-			}
-		} else {
-			//single result - object
-			html += parseFormat(firstChild);
-		}
+        //loop through all YQL return elements and result replace regex
+        if (firstChild.length !== undefined){
+            //multiple results - array
+            for(var i = 0; i < firstChild.length; i++){
+                html += parseFormat(firstChild[i]);
+            }
+        } else {
+            //single result - object
+            html += parseFormat(firstChild);
+        }
 		
-		document.getElementById(queryInsert).innerHTML = html;
-		yqlWidget.render();
-	}
+        document.getElementById(queryInsert).innerHTML = html;
+        yqlWidget.render();
+    }
 	
-	/************************************************************
-	* Method: Parse Format
-	* Description: Loop through format array for provided
-	*              data set node
-	************************************************************/
-	var parseFormat = function(node){
-		currString = node;
+    /************************************************************
+    * Method: Parse Format
+    * Description: Loop through format array for provided
+    *              data set node
+    ************************************************************/
+    var parseFormat = function(node){
+        currString = node;
 		
-		console.log(currString);
+        if (setupConfig['debug'] && window.console){ console.log(currString); }
 		
-		//replace YQL result placeholders with return content
-		if (resultFormat){ currString = resultFormat.replace(regex, function(matchedSubstring, index, originalString){
-			return eval("currString." + index);
-		});}
+        //replace YQL result placeholders with return content
+        if (resultFormat){ currString = resultFormat.replace(regex, function(matchedSubstring, index, originalString){
+            return eval("currString." + index);
+        });}
 		
-		return currString;
-	}
+        return currString;
+    }
 
-	/************************************************************
-	* Method: Public Function Return
-	* Functions: init - starts yql parsing functions
-	*			 getYQLDataCallback - yql run callback
-	************************************************************/
-	return {
-		//push widget on the load stack
-		push: function(query, config, format, insertEl){
-			//validate widget variables
-			if (query == null || format == null || insertEl == null){
-				if (setupConfig['debug'] && window.console){ console.log('Missing query, return format or insert element'); }
-				return null;
-			}
+    /************************************************************
+    * Method: Public Function Return
+    * Functions: init - starts yql parsing functions
+    *			 getYQLDataCallback - yql run callback
+    ************************************************************/
+    return {
+        //push widget on the load stack
+        push: function(query, config, format, insertEl){
+            //validate widget variables
+            if (query == null || format == null || insertEl == null){
+                if (setupConfig['debug'] && window.console){ console.log('Missing query, return format or insert element'); }
+                return null;
+            }
 			
-			//push widget load on the stack
-			widgetStack.push(function(){ yqlWidget.init(query, config, format, insertEl); });
-		},
+            //push widget load on the stack
+            widgetStack.push(function(){ yqlWidget.init(query, config, format, insertEl); });
+        },
 		
-		//pop widget off the load stack and execute
-		render: function(){ if (widgetStack.length > 0){ widgetStack.pop()(); } },
+        //pop widget off the load stack and execute
+        render: function(){ if (widgetStack.length > 0){ widgetStack.pop()(); } },
 	
-		//widget initialization
-		init: function(query, config, format, insertEl){ 
-			resultFormat = format; queryInsert = insertEl;
-			if (config){ setupConfig = config; }
-			return getYQLData(query);
-		}, 
+        //widget initialization
+        init: function(query, config, format, insertEl){ 
+            resultFormat = format; queryInsert = insertEl;
+            if (config){ setupConfig = config; }
+            return getYQLData(query);
+        }, 
 		
-		//yql data caption success callback
-		getYQLDataCallback: function(o){
-			console.log(o.query);
-			if (! o.query){
-				if (setupConfig['debug'] && window.console){ console.log('YQL query returned no results'); }
-				return null;
-			}
-			parseYQLResults(o.query.results);
-		}
-	}
+        //yql data caption success callback
+        getYQLDataCallback: function(o){
+            if (setupConfig['debug'] && window.console){ console.log(o.query); }
+            if (! o.query){
+                if (setupConfig['debug'] && window.console){ console.log('YQL query returned no results'); }
+                return null;
+            }
+            parseYQLResults(o.query.results);
+        }
+    }
 }();
